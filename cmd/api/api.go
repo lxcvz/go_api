26a2e19731784338876bfecfbb6d21288598095a
Lucas <lucas-mateus.dc@hotmail.com>
2025-01-17 15:53:27 -0300

    initial commit

---
+++ cmd/api/api.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+type config struct {
+	addr string
+}
+
+func (app *application) run() error {
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Starting server on %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module lx
+
+go 1.23.5
