e1851f51a02b3e33a4abffd373bdb97700a2f6f3
Lucas <lucas-mateus.dc@hotmail.com>
2025-01-18 23:44:41 -0300

    feat(db): added initial db template

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"log"
+	"lx/internal/db"
 	"lx/internal/env"
 	"lx/internal/store"
 )
 
 func main() {
-
-	store := store.NewStorage(nil)
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://lucas:adminpassword@localhost:5432/lx?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 25),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 25),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Fatalf("db.New: %v", err)
+	}
+
+	defer db.Close()
+	log.Println("Database connected")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
